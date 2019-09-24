4a807073f3afdb0ea7993e57baa6aa802f183d8e
ezquire <tyler.gearing@gmail.com>
2019-09-24 14:17:05 -0700

    getting started with node

---
+++ node/MyShop/listProducts.js
@@ -0,0 +1,10 @@
+var faker = require('faker');
+
+console.log("===================");
+console.log("WELCOME TO MY SHOP!");
+console.log("===================");
+
+for(var i = 0; i < 10; ++i) {
+    var str = faker.commerce.productName() + " - " + "$" + faker.commerce.price();
+    console.log(str);
+}
